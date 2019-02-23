0e7c213221798673b8aff64ed20e4f4a460c19af
Steve Winton <stevewinton@gmail.com>
2019-02-22 18:51:54 -0600

    Add installation_access_token template tag

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const App = require('@octokit/app');
 
+const getPrivateKey = (path) => fs.readFileSync(path);
+
 module.exports.templateTags = [
   {
     name: 'jwt',
@@ -8,7 +10,7 @@ module.exports.templateTags = [
     description: 'Generates a JSON Web Token, allowing you to authenticate with the GitHub API as your GitHub App',
     args: [
       {
-        displayName: 'ID',
+        displayName: 'App ID',
         description: 'ID of GitHub App',
         type: 'number'
       },
@@ -19,9 +21,34 @@ module.exports.templateTags = [
       }
     ],
     async run(context, id, path) {
-      const privateKey = fs.readFileSync(path);
-      const app = new App({ id, privateKey });
+      const app = new App({ id, privateKey: getPrivateKey(path) });
       return app.getSignedJsonWebToken();
+    }
+  },
+  {
+    name: 'installation_access_token',
+    displayName: 'Installation Access Token',
+    description: 'Generates an Installation Access Token, allowing you to authenticate with the GitHub API as an installation of your GitHub App',
+    args: [
+      {
+        displayName: 'App ID',
+        description: 'ID of GitHub App',
+        type: 'number'
+      },
+      {
+        displayName: 'Private key file',
+        description: 'Path to private key file in PEM format',
+        type: 'string'
+      },
+      {
+        displayName: 'Installation ID',
+        description: 'ID of GitHub App Installation',
+        type: 'number'
+      }
+    ],
+    async run(context, id, path, installationId) {
+      const app = new App({ id, privateKey: getPrivateKey(path) });
+      return app.getInstallationAccessToken({ installationId })
     }
   }
 ];
